729130e84f6adef65aad6949f6d9b7b220b2d035
Thanh Hai <nth2355@gmail.com>
2026-03-11 11:10:18 +0700

    feat: create main screen file and create app routes

---
+++ lib/features/weather/presentation/screens/cities_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CitiesScreen extends StatelessWidget {
+  const CitiesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
