a279a04ba320b78dda02b3c7b4b603aaf81fb4bf
nghiahoangdever <hoangdainghia1818@gmail.com>
2026-03-13 10:48:21 +0700

    thêm cities_screen và sửa một số icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-import '../core/constants.dart';
-import '../features/weather/data/weather_api_service.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await dotenv.load(fileName: ".env");
-
-  // Khởi tạo Service
-  final weatherService = WeatherApiService();
-
-  print('BẮT ĐẦU TEST API ');
-
-  try {
-    final data = await weatherService.getWeather(
-      ApiConstants.defaultLat,
-      ApiConstants.defaultLon,
-    );
-
-    print('Kết quả thành công:');
-    print('Thành phố: ${data.location.name}');
-    print('Nhiệt độ: ${data.current.temp}°C');
-  } catch (e) {
-    print('Lỗi khi call API: $e');
-  }
-
-  print(' KẾT THÚC TEST');
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('Weather App Test')),
-      ),
-    );
-  }
+void main() {
+  testWidgets('App builds successfully', (WidgetTester tester) async {
+    expect(true, isTrue);
+  });
 }
